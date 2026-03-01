b628de38a27a6e64078e5e45760c452ee81210ea
Arif Narayana <marcella_pratiknyo@sman45-jkt.sch.id>
2026-03-01 16:25:16 +0700

    CQC Config: Add company info fields for invoice

--- config/businesses/cqc.php
+++ config/businesses/cqc.php
@@ -4,7 +4,21 @@ return [
     'name' => 'CQC Enjiniring',
     'business_type' => 'contractor',
     'database' => 'adf_cqc',
-    'logo' => '',
+    
+    // Company Info for Invoice
+    'logo' => 'cqc-logo.png', // Place logo file in /uploads/ folder
+    'tagline' => 'Solar Panel Installation Contractor',
+    'address' => 'Jl. Example No. 123', // UPDATE THIS
+    'city' => 'Jakarta', // UPDATE THIS
+    'phone' => '+62 21 1234567', // UPDATE THIS
+    'email' => 'info@cqc-engineering.com', // UPDATE THIS
+    'npwp' => '00.000.000.0-000.000', // UPDATE THIS
+    
+    // Bank Info for Invoice Payment
+    'bank_name' => 'Bank Mandiri', // UPDATE THIS
+    'bank_account' => '1234567890', // UPDATE THIS
+    'bank_holder' => 'PT CQC Enjiniring', // UPDATE THIS
+    
     'enabled_modules' => [
         'cashbook',
         'auth',
